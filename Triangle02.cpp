a04444bd97fb605cc04c0351c2d693ff308eb534
HARIHARAN A <95291060+hari9608@users.noreply.github.com>
2023-02-02 21:11:54 +0530

    Create Triangle02.cpp

---
+++ Triangle02.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "ENTER THE NUMBER OF N VALUE: ";
+    cin >> n;
+    for(int row=1; row<=n; row++)
+    {
+        for(int col=row; col>=1; col--)
+        {
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+/*
+ENTER THE NUMBER OF N VALUE: 5
+1
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
+*/
